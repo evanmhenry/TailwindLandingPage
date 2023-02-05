23c956c1e365b76acb0afc98e4c386182d1970b4
Evan Henry <evan@spadehq.com>
2023-02-05 13:20:53 -0500

    nav bar built

--- tailwind-landing-page/src/App.jsx
+++ tailwind-landing-page/src/App.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react"
+import styles from "./styles"
+import {
+  Billing,
+  Business,
+  Button,
+  CardDeal,
+  Clients,
+  CTA,
+  Feedback,
+  Footer,
+  GetStarted,
+  Hero,
+  Navbar,
+  Stats,
+  Testimonials,
+} from "./components"
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar></Navbar>
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero></Hero>
+        </div>
+      </div>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
